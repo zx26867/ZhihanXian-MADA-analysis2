01a669430b83e3048fb09615a30d05976a0bddb5
Zhihan Xian <zx26867@uga.edu>
2021-09-15 00:25:44 -0400

    cleaned data attempt1

--- code/processing_code/processingscript.R
+++ code/processing_code/processingscript.R
@@ -8,10 +8,10 @@
 library(readxl) #for loading Excel files
 library(dplyr) #for data processing
 library(here) #to set paths
-
+library(tidyverse)
 #path to data
 #note the use of the here() package and not absolute paths
-data_location <- here::here("data","raw_data","exampledata.xlsx")
+data_location <- here::here("data","raw_data","Impaired_Driving_2012.csv")
 
 #load data. 
 #note that for functions that come from specific packages (instead of base R)
@@ -19,37 +19,18 @@ data_location <- here::here("data","raw_data","exampledata.xlsx")
 #package::function() that's not required one could just call the function
 #specifying the package makes it clearer where the function "lives",
 #but it adds typing. You can do it either way.
-rawdata <- readxl::read_excel(data_location)
+rawdata <- read.csv(data_location)
 
 #take a look at the data
 dplyr::glimpse(rawdata)
 
-#dataset is so small, we can print it to the screen.
-#that is often not possible.
-print(rawdata)
+# data cleaning
 
-# looks like we have measurements for height (in centimeters) and weight (in kilogram)
-
-# there are some problems with the data: 
-# There is an entry which says "sixty" instead of a number. 
-# Does that mean it should be a numeric 60? It somehow doesn't make
-# sense since the weight is 60kg, which can't happen for a 60cm person (a baby)
-# Since we don't know how to fix this, we need to remove the person.
-# This "sixty" entry also turned all Height entries into characters instead of numeric.
-# We need to fix that too.
-# Then there is one person with a height of 6. 
-# that could be a typo, or someone mistakenly entered their height in feet.
-# Since we unfortunately don't know, we'll have to remove this person.
-# similarly, there is a person with weight of 7000, which is impossible,
-# and one person with missing weight.
-# to be able to analyze the data, we'll remove those 5 individuals
-
-# this is one way of doing it. Note that if the data gets updated, 
-# we need to decide if the thresholds are ok (newborns could be <50)
-
-processeddata <- rawdata %>% dplyr::filter( Height != "sixty" ) %>% 
-                             dplyr::mutate(Height = as.numeric(Height)) %>% 
-                             dplyr::filter(Height > 50 & Weight < 1000)
+# death rate has to be a number btw 0 and 100
+processeddata <- rawdata %>% dplyr::filter( `All.Ages..2012` > 0 & `All.Ages..2012` < 100) %>% 
+                             dplyr::filter( `Female..2012` > 0 & `Female..2012` < 100)
+# drop rows with NA in any entry
+processeddata <- processeddata %>% drop_na()
 
 # save data as RDS
 # I suggest you save your processed and cleaned data as RDS or RDA/Rdata files. 
